0b7e1eae876a2e93d450459183f118e8279e717f
injecteer <injecteer@gmail.com>
2015-03-30 15:56:33 +0200

    no message

--- src/com/commons/android/BaseUtils.java
+++ src/com/commons/android/BaseUtils.java
@@ -2,9 +2,6 @@ package com.commons.android;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.NumberFormat;
@@ -305,54 +302,4 @@ public class BaseUtils {
     return l;
   }
 
-  /**
-   * On Android 3.0 and above, while using the ActionBar tabbed navigation style, the tabs sometimes appear above the action bar.
-   * This helper method allows you to control the 'hasEmbeddedTabs' behaviour.
-   * A value of true will put the tabs inside the ActionBar, a value of false will put it above or below the ActionBar.
-   *
-   * You should call this method while initialising your ActionBar tabs.
-   * Don't forget to also call this method during orientation changes (in the onConfigurationChanged() method).
-   *
-   * @param inActionBar
-   * @param inHasEmbeddedTabs
-   */
-  public static void setHasEmbeddedTabs( Object inActionBar, final boolean inHasEmbeddedTabs ) {
-    // get the ActionBar class
-    Class<?> actionBarClass = inActionBar.getClass();
-
-    // if it is a Jelly Bean implementation (ActionBarImplJBMR2), get the super
-    // super class (ActionBarImplICS)
-    if( "android.support.v7.app.ActionBarImplJBMR2".equals( actionBarClass.getName() ) ){
-      actionBarClass = actionBarClass.getSuperclass().getSuperclass();
-    }else if( "android.support.v7.app.ActionBarImplJB".equals( actionBarClass.getName() ) ){
-    // if it is a Jelly Bean implementation (ActionBarImplJB), get the super
-    // class (ActionBarImplICS)
-      actionBarClass = actionBarClass.getSuperclass();
-    }
-
-    try{
-      // try to get the mActionBar field, because the current ActionBar is
-      // probably just a wrapper Class
-      // if this fails, no worries, this will be an instance of the native
-      // ActionBar class or from the ActionBarImplBase class
-      final Field actionBarField = actionBarClass.getDeclaredField( "mActionBar" );
-      actionBarField.setAccessible( true );
-      inActionBar = actionBarField.get( inActionBar );
-      actionBarClass = inActionBar.getClass();
-    }catch( IllegalAccessException | IllegalArgumentException | NoSuchFieldException e ){
-    }
-
-    try{
-      // now call the method setHasEmbeddedTabs, this will put the tabs inside
-      // the ActionBar
-      // if this fails, you're on you own <img
-      // src="http://www.blogc.at/wp-includes/images/smilies/icon_wink.gif"
-      // alt=";-)" class="wp-smiley">
-      final Method method = actionBarClass.getDeclaredMethod( "setHasEmbeddedTabs", new Class[] { Boolean.TYPE } );
-      method.setAccessible( true );
-      method.invoke( inActionBar, new Object[] { inHasEmbeddedTabs } );
-    }catch( NoSuchMethodException | InvocationTargetException | IllegalAccessException | IllegalArgumentException e ){
-    }
-  }
-  
 }
